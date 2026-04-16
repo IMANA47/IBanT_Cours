2316468f9b56f8654f6399c0e862946ad2b26a0c
Francois Nsengimana <francknsengimana@gmail.com>
2026-04-16 16:06:33 +0100

    adds the AI project for the AI agent creation mini course

---
+++ Projet/Python/Pizzas/main.py
@@ -0,0 +1,12 @@
+"""
+auth
+"""
+
+
+def afficher(collection):
+    #" -----LISTE DES PIZZQ (4)-----"
+    print("Liste des piza".len(collection))
+
+pizza =("4 fromages", "vegetarienne", "hawai", "calzone")
+print(afficher(pizza))
+
