c7faa0f98d6003fe0b788a62f2296e5a4fb899d3
Francois Nsengimana <francknsengimana@gmail.com>
2026-06-16 14:08:37 +0100

    add code for project

---
+++ IOT/TP-POT/code/sketch_jun16a/sketch_jun16a.ino
@@ -0,0 +1,13 @@
+int R1=13;
+float VIN=A0;
+int N;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(R1,OUTPUT);
+pinMode(VIN,INPUT)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+VIN= 5*
+}
