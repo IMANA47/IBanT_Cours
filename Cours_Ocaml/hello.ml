e135fc4ec1b0370f15129399436109a21e917c72
Francois Nsengimana <francknsengimana@gmail.com>
2026-07-02 13:56:09 +0100

    update execution counts and modify output values in Python notebook on functions and ocaml programme

--- Cours_Ocaml/hello.ml
+++ Cours_Ocaml/hello.ml
@@ -1,1 +1,15 @@
+let x1 = ref 1;;
+print_int !x1;;
+print_string "\n";;
+x1:= !x1 + 3;;
+print_int !x1;;
+print_string "\n";;
+
 let () = print_endline "Hello, World!"
+let x = 12
+let y = 5
+let z = x + y
+let () = Printf.printf "La somme de %d et %d est %d\n" x y z
+
+
+
